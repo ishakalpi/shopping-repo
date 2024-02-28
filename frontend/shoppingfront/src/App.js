920df9140302ce1a3f7612ff3afee9db1492c8d5
ishakalpi <153492730+ishakalpi@users.noreply.github.com>
2024-02-28 17:29:24 +0530

    add nav bar , home , cart pages

--- frontend/shoppingfront/src/App.js
+++ frontend/shoppingfront/src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import {BrowserRouter ,Route, Routes } from "react-router-dom"
+import NavBar from "./components/NavBar";
+import Cart from "./components/Cart";
+import Home from "./components/Home";
+
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <NavBar/>
+      <Routes>
+        <Route path="/cart" Component={Cart}/>
+        <Route path="/" Component={Home}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
