d23863ac82d36f6c304a28ee8c9c8196c8f8ef7e
ishakalpi <153492730+ishakalpi@users.noreply.github.com>
2024-03-02 18:21:35 +0530

    decresed quantity , remove product from cart and other modifications

--- frontend/shoppingfront/src/features/cartSlice.js
+++ frontend/shoppingfront/src/features/cartSlice.js
@@ -34,9 +34,52 @@ const cartSlice = createSlice({
             }
           localStorage.setItem("cartItems" , JSON.stringify(state.cartItems));
         },
+        removeFromCart(state, action)
+        {
+            const nextCartItems = state.cartItems.filter(
+                (cartItem)=> cartItem.id !== action.payload.id
+            )
+
+            state.cartItems = nextCartItems;
+            localStorage.setItem("cartItems" , JSON.stringify(state.cartItems))
+
+            toast.error(action.payload.name + " Remove From Cart", 
+                {
+                    position:"bottom-left",
+                });
+        }, 
+        decreaseCart(state,action){
+            const itemIndex = state.cartItems.findIndex(
+                cartItem =>  cartItem.id === action.payload.id
+            )
+            if(state.cartItems[itemIndex].cartQuantity >1)
+             {
+                state.cartItems[itemIndex].cartQuantity -= 1
+
+                toast.info("Decreased " +action.payload.name + "From Cart", 
+                {
+                    position:"bottom-left",
+                });
+            }
+            else if (state.cartItems[itemIndex].cartQuantity  ===  1)
+            {
+                const nextCartItems = state.cartItems.filter(
+                    (cartItem)=> cartItem.id !== action.payload.id
+                )
+    
+                state.cartItems = nextCartItems;
+                
+    
+                toast.error(action.payload.name + " Remove From Cart", 
+                    {
+                        position:"bottom-left",
+                    });
+            } 
+            localStorage.setItem("cartItems" , JSON.stringify(state.cartItems))
+        },
     },
 });
- 
-export const { addToCart } =  cartSlice.actions;
+
+export const { addToCart,removeFromCart, decreaseCart } =  cartSlice.actions;
 
 export default cartSlice.reducer;
